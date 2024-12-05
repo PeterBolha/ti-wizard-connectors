8276f0734b7c90b780052d42678c35672d6a6a46
peterbolha <xbolha@fi.muni.cz>
2024-12-05 16:17:39 +0100

    feat: git versioning

--- config_version_managers/config_version_manager_initializer.py
+++ config_version_managers/config_version_manager_initializer.py
@@ -1,6 +1,8 @@
 from config_version_managers.config_version_manager import ConfigVersionManager
-from config_version_managers.local_config_version_manager import \
-    LocalConfigVersionManager
+from config_version_managers.git_config_version_manager import GitConfigVersionManager
+from config_version_managers.local_config_version_manager import (
+    LocalConfigVersionManager,
+)
 from enums.config_version_manager_type import ConfigVersionManagerType
 
 
@@ -10,12 +12,8 @@ class ConfigVersionManagerInitializer:
 
     def get_config_version_manager(self) -> ConfigVersionManager:
         try:
-            version_manager_type_in_cfg = self.__VERSION_MANAGER_CFG.get(
-                "type"
-            )
-            version_manager_type = ConfigVersionManagerType[
-                version_manager_type_in_cfg
-            ]
+            version_manager_type_in_cfg = self.__VERSION_MANAGER_CFG.get("type")
+            version_manager_type = ConfigVersionManagerType[version_manager_type_in_cfg]
         except KeyError:
             raise ValueError(
                 f"Invalid version manager type '"
@@ -27,3 +25,5 @@ class ConfigVersionManagerInitializer:
         match version_manager_type:
             case ConfigVersionManagerType.LOCAL:
                 return LocalConfigVersionManager(self.__VERSION_MANAGER_CFG)
+            case ConfigVersionManagerType.GIT:
+                return GitConfigVersionManager(self.__VERSION_MANAGER_CFG)
---
+++ config_version_managers/file_config_version_manager.py
@@ -0,0 +1,67 @@
+import json
+from abc import abstractmethod
+from typing import Dict, Any
+
+import yaml
+
+from config_version_managers.config_version_manager import ConfigVersionManager
+from enums.config_file_format import ConfigFileFormat
+
+
+class FileConfigVersionManager(ConfigVersionManager):
+    def __init__(self, file_version_manager_cfg):
+        self._CONFIG_FILE_NAME = file_version_manager_cfg.get("config_file_name")
+
+        try:
+            config_file_format_in_cfg = file_version_manager_cfg.get(
+                "config_file_format"
+            )
+            config_file_format = ConfigFileFormat[config_file_format_in_cfg]
+        except KeyError:
+            raise ValueError(
+                f"Invalid config file format '{config_file_format_in_cfg}' in "
+                f"configuration. "
+                f"Allowed values are: "
+                f"{', '.join([e.name for e in ConfigFileFormat])}"
+            )
+
+        self._CONFIG_FILE_FORMAT = config_file_format
+
+    def save_config_txt(self, config, file_path: str) -> str:
+        file_path += ".txt"
+        with open(file_path, "w") as txt_file:
+            txt_file.write(str(config))
+        return file_path
+
+    def save_config_json(self, config, file_path: str) -> str:
+        file_path += ".json"
+        with open(file_path, "w") as json_file:
+            json.dump(config, json_file, indent=4)
+        return file_path
+
+    def save_config_yaml(self, config, file_path: str) -> str:
+        file_path += ".yaml"
+        with open(file_path, "w") as yaml_file:
+            yaml.dump(config, yaml_file, default_flow_style=False, sort_keys=False)
+        return file_path
+
+    def save_config_to_file(
+        self,
+        file_path: str,
+        config: Dict[str, Any],
+        output_format: ConfigFileFormat = None,
+    ) -> str:
+        if not output_format:
+            output_format = self._CONFIG_FILE_FORMAT
+
+        match output_format:
+            case output_format.TXT:
+                return self.save_config_txt(config, file_path)
+            case output_format.JSON:
+                return self.save_config_json(config, file_path)
+            case output_format.YAML:
+                return self.save_config_yaml(config, file_path)
+
+    @abstractmethod
+    def save_configuration(self, config: dict[str, Any]) -> None:
+        pass
